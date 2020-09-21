8afa636df6f5b0053f8b9147adabee32fcb63e5e
Leonardo do Nascimento <leonardo3.nascimento@gmail.com>
2020-09-21 12:38:00 -0300

    style: identaçao da home

--- web/src/pages/Home/index.tsx
+++ web/src/pages/Home/index.tsx
@@ -1,5 +1,6 @@
 import React from 'react';
 import { FiLogIn, FiShoppingCart } from 'react-icons/fi';
+import { Link } from 'react-router-dom';
 
 import logoImg from '../../assets/images/logo.svg';
 import notesImg from '../../assets/images/notes.png';
@@ -13,35 +14,41 @@ import './styles.css';
 function Home() {
   return (
     <div id="page-home" className="container">
+
       <div className="top-bar-container">
-          <span />
-          <div className="top-bar-session">
-            <a href="http://localhost:3000/">Carrinho <FiShoppingCart /></a>
-            <a href="http://localhost:3000/">Login <FiLogIn /></a>
-          </div>
+        <span />
+        <div className="top-bar-session">
+          <Link to="/cart">
+            Carrinho <FiShoppingCart />
+          </Link>
+          <Link to="/login">
+            Login <FiLogIn />
+          </Link>
         </div>
-        <header className="page-header">
+      </div>
 
+      <header className="page-header">
+        <img src={notesImg} alt="postite"/>
+        <div className="logo-container">
+          <h2>Sua vida mais planejada com fofuras</h2>
+          <img src={logoImg} alt="Alira Notes"/>
+        </div>
+      </header>
 
-          <img src={notesImg} alt="postite"/>
-          <div className="logo-container">
-            <h2>Sua vida mais planejada com fofuras</h2>
-            <img src={logoImg} alt="Alira Notes"/>
-          </div>
-
-
-        </header>
       <nav className="nav-bar-container">
-          <a href="http://localhost:3000/">Início</a>
-          <a href="http://localhost:3000/">Bloquinhos</a>
-          <a href="http://localhost:3000/">Cadernos</a>
-          <a href="http://localhost:3000/">Adesivos</a>
-          <a href="http://localhost:3000/">Quem Somos</a>
-        </nav>
+          <Link to="/">Início</Link>
+          <Link to="/">Bloquinhos</Link>
+          <Link to="/">Cadernos</Link>
+          <Link to="/">Adesivos</Link>
+          <Link to="/about">Quem Somos</Link>
+      </nav>
+
       <div className="banner">
         <img src={bannerImg} alt="banner"/>
       </div>
+
       <h1>PRODUTOS</h1>
+
       <div id="page-home-content">
 
         <table><tr><td>Produto</td></tr></table>
@@ -54,13 +61,14 @@ function Home() {
         <table><tr><td>Produto</td></tr></table>
 
       </div>
+
       <footer id="footer">
         <div className="upper-footer">
           <div className="links">
-            <a href="http://localhost:3000/">Quem Somos</a>
-            <a href="http://localhost:3000/">Pagamento e envio de produtos</a>
-            <a href="http://localhost:3000/">Trocas e devoluções de produtos</a>
-            <a href="http://localhost:3000/">Contato</a>
+            <Link to="/about">Quem Somos</Link>
+            <Link to="/payments">Pagamento e envio de produtos</Link>
+            <Link to="/exchanges">Trocas e devoluções de produtos</Link>
+            <Link to="/contact">Contato</Link>
           </div>
           <div className="social-media">
             <h3>Redes Sociais</h3>
@@ -70,14 +78,13 @@ function Home() {
             </div>
           </div>
         </div>
+
         <div className="lower-footer">
           <p>Alira notes - papelaria criativa<br />
           CNPJ: 11.222.333/0001-44<br />
           São Carlos - SP - 2020</p>
         </div>
-
       </footer>
-
 
     </div>
   );
