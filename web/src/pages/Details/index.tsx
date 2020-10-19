5f6d64e4545a2038cfc24e610011ce63a50e26e8
Leonardo do Nascimento <leonardo3.nascimento@gmail.com>
2020-10-19 01:25:50 -0300

    chore: list of products detailed

--- web/src/pages/Details/index.tsx
+++ web/src/pages/Details/index.tsx
@@ -1,20 +1,53 @@
-import React from 'react';
-import { useHistory } from 'react-router-dom';
+import React, { useEffect, useState } from 'react';
+import { useHistory, useParams } from 'react-router-dom';
 
 import Footer from '../../components/Footer';
 import PageHeader from '../../components/PageHeader';
 import ImagesCarousel from '../../components/ImagesCarousel';
+import Product from '../../components/Product';
+import api from '../../services/api';
 
 import aliraNotesIcon from '../../assets/images/icons/cat.svg';
 
 import './styles.css';
 
+interface Product {
+  code: string;
+  category: string;
+  name: string;
+  description: string;
+  stock: number;
+  price: number;
+  images: Array<{
+    url: string;
+  }>;
+}
+
+interface ProductParams {
+  id: string;
+}
+
 function Details() {
 
+  const params = useParams<ProductParams>();
   const history = useHistory();
 
-  function handleWantProduct() {
+  const [product,setProduct] = useState<Product>();
 
+  console.log('product', params);
+
+
+  useEffect(() => {
+     api.get(`/products/${params.id}`).then(response => {
+      setProduct(response.data);
+    })
+  },[params.id]);
+
+  if(!product) {
+    return <p>Carregando...</p>
+  }
+
+  function handleWantProduct() {
     history.push('/cart');
   }
 
@@ -25,40 +58,26 @@ function Details() {
       <main>
         <article className="detail-item">
           <header>
-            <h2>Bloco de anotações com linhas tipo postite colorido</h2>
+            <h2>{product.name}</h2>
           </header>
 
           <div className="detail-content">
             <div className="detail-preview">
-              <ImagesCarousel />
+              <ImagesCarousel images={product.images} />
             </div>
             <div className="detail-description">
               <h4>DESCRIÇÃO DO PRODUTO</h4>
 
               <div className="detail-background">
-                <p>Bloco de notas adesivos para escola e escritório,
-                  material de papelaria com linhas, notas adesivas.<br /><br />
-                  Material Papel,
-                  Tamanho: cerca de 7.2x7cm,
-                  Estilo de cor: mármore, cinza de cimento, vermelho listrado,
-                  Páginas: cerca de 75 folhas (pode haver 1-3 folhas de erro)
-                  Nome da etiqueta: almofada de memorando da textura da natureza
-
-                  Bloco de notas adesivos para escola e escritório,
-                  material de papelaria com linhas, notas adesivas.
-                  Bloco de notas adesivos para escola e escritório,
-                  material de papelaria com linhas, notas adesivas.<br /><br />
-                  Material Papel,
-                  Tamanho: cerca de 7.2x7cm,
-                  Estilo de cor: mármore, cinza de cimento, vermelho listrado,
-                  Páginas: cerca de 75 folhas (pode haver 1-3 folhas de erro)
-                  Nome da etiqueta: almofada de memorando da textura da natureza
-                  Bloco de notas adesivos para escola e escritório,
-                  material de papelaria com linhas, notas adesivas.
-                </p>
+                <p>{product.description}</p>
                 <div>
                   <img src={aliraNotesIcon} alt="logo"/>
-                  <span>R$19,90</span>
+                  <span>
+                  { Intl.NumberFormat('pt-BR' , {
+                    style: 'currency',
+                    currency: 'BRL'
+                    }).format(product.price) }
+                  </span>
                 </div>
               </div>
 
