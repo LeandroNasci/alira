d1dbe736d95f016b6528506413c515794d7c9182
Leonardo do Nascimento <leonardo3.nascimento@gmail.com>
2020-11-05 01:25:01 -0300

    chore: add cross-env and pg compatibility

--- server/src/database/connection.ts
+++ server/src/database/connection.ts
@@ -2,11 +2,31 @@ import knex from 'knex';
 import path from 'path';
 import 'dotenv/config'
 
-let db;
+let db: knex<any, unknown[]>
 
 switch (process.env.NODE_ENV) {
+  case 'development':
+    db = knex({
+      client: 'sqlite3',
+      connection: {
+        filename: path.resolve(__dirname, 'database.sqlite')
+      },
+      useNullAsDefault: true,
+    });
+    break;
+
+ case 'test':
+    db = knex({
+      client: 'sqlite3',
+      connection: {
+        filename: path.resolve(__dirname, 'teste.sqlite')
+      },
+      useNullAsDefault: true,
+    });
+    break;
 
   case 'production':
+  default:
     db = knex({
       client: 'pg',
       connection: {
@@ -19,28 +39,6 @@ switch (process.env.NODE_ENV) {
     })
     break;
 
-  case 'development':
-    db = knex({
-      client: 'sqlite3',
-      connection: {
-        filename: path.resolve(__dirname, 'database.sqlite')
-      },
-      useNullAsDefault: true,
-    });
-    break;
-
-  case 'test':
-    db = knex({
-      client: 'sqlite3',
-      connection: {
-        filename: path.resolve(__dirname, 'teste.sqlite')
-      },
-      useNullAsDefault: true,
-    });
-    break;
-
-  default:
-    break;
 }
 
 export default db;
--- server/src/routes.ts
+++ server/src/routes.ts
@@ -9,12 +9,12 @@ import OrderController from './controllers/OrderController';
 const routes = Router();
 const upload = multer(uploadConfig);
 
-routes.get('/products', ProductsController.index ); //mostrar todos os produtos
-routes.get('/products/:id', ProductsController.show ); //mostrar um unico produtos detalhado
-routes.post('/products', upload.array('images'), ProductsController.create);//adicionar novo produto ao estoque
-// routes.put('/products');//atualiza estoque apos uma compra
-// routes.delete('/products/:id');//retira do estoque um produto
+routes.get('/products', ProductsController.index );                             //mostrar todos os produtos
+routes.get('/products/:id', ProductsController.show );                          //mostrar um unico produtos detalhado
+routes.post('/products', upload.array('images'), ProductsController.create);    //adicionar novo produto ao estoque
+routes.put('/products', ProductsController.update);                             //atualiza estoque apos uma compra
+routes.delete('/products/:id', ProductsController.delete);                      //retira do estoque um produto
 
-routes.post('/checkout', OrderController.create ); //finalizar a compra
+routes.post('/checkout', OrderController.create );                              //finalizar a compra
 
 export default routes;
