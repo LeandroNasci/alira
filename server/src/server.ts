bfbd51a790ac2c362af5397fe3e9c49d2b00541d
Leonardo do Nascimento <leonardo3.nascimento@gmail.com>
2020-12-28 01:11:40 -0300

    test(server): cors with origin

--- server/src/server.ts
+++ server/src/server.ts
@@ -1,19 +1,35 @@
-import 'dotenv/config';
-
 import express from 'express';
 import path from 'path';
 import cors from 'cors';
-import routes from './routes';
 import morgan from 'morgan';
 
+import routes from './routes';
+import errorHandler from './errors/handler';
 
 import 'express-async-errors'
-import errorHandler from './errors/handler';
+import 'dotenv/config';
+
 
 const app = express();
 
-
-app.use(cors({ origin: 'http://localhost:3000' }));
+var allowedOrigins = [
+  'http://localhost:3000',
+  'https://alira.vercel.app',
+  'http://sandbox.pagseguro.uol.com.br',
+  'https://sandbox.pagseguro.uol.com.br',
+];
+app.use(cors({
+  origin: function(origin, callback){
+    // allow requests with no origin (like mobile apps or curl requests)
+    if(!origin) return callback(null, true);
+    if(allowedOrigins.indexOf(origin) === -1){
+      var msg = `The CORS policy for ${origin} does not ` +
+                'allow access from the specified Origin.';
+      return callback(new Error(msg), false);
+    }
+    return callback(null, true);
+  }
+}));
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 app.use(routes);
